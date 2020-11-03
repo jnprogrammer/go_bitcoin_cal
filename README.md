2c6ca7a2cadb35ba6be8c50756c67308971aa5e6
j <joshuanparker@gmail.com>
2020-11-02 23:56:21 -0500

    First push.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # go_bitcoin_cal
-A Bitcoin calculator
+A Bitcoin calculator that will break down bitcoin totals
+entered into its lower units of satoshi's  
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+var btc uint32
+var ada uint32
+
+func main() {
+	btc = 100000000
+	ada = 1000000
+
+	fmt.Printf("BTC = %d\n", btc)
+	fmt.Printf("ADA = %d\n", ada)
+}
